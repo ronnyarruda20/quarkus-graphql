3ac5107073b845a92f0cc7e6a1af8bd64fff0051
Ronny Arruda <ronnyapinho@gmail.com>
2021-10-24 11:39:00 -0400

    Implementação de usuario

--- src/main/java/org/graphql/modules/model/Usuario.java
+++ src/main/java/org/graphql/modules/model/Usuario.java
@@ -1,4 +1,17 @@
 package org.graphql.modules.model;
 
-public class Usuario implements Pessoa {
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@Setter
+public class Usuario extends PanacheEntity implements Pessoa {
+    private String nome;
+    private String endereco;
+    private String email;
+    private Byte receita;
 }
