a17fbc9f9a983b4af1ff44752af2b1b7265022d6
Ronny Arruda <ronnyapinho@gmail.com>
2021-10-24 11:38:42 -0400

    Implementação de medicamento

--- src/main/java/org/graphql/modules/model/Medicamento.java
+++ src/main/java/org/graphql/modules/model/Medicamento.java
@@ -1,4 +1,21 @@
 package org.graphql.modules.model;
 
-public class Medicamento {
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Medicamento extends PanacheEntity {
+    private String nome;
+    private String bula;
+    private Integer quantidade;
+    private Byte receitaUsuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "estoque_id", foreignKey = @ForeignKey(name = "fk_estoque_id"))
+    private Estoque estoque;
 }
